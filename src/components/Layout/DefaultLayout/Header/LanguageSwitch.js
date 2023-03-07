720bbb6da2dfe26510f3b9da8a1ba450432413d4
Huy Nguyen <luonghuy0406@gmail.com>
2023-03-07 23:02:04 +0700

    change langugage

--- src/components/Layout/DefaultLayout/Header/LanguageSwitch.js
+++ src/components/Layout/DefaultLayout/Header/LanguageSwitch.js
@@ -2,24 +2,30 @@ import { IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Tooltip, Typogr
 import React, { useState } from "react";
 import en from "./us.png";
 import vi from "./vietnam.png";
+import { useTranslation } from 'react-i18next'
 
 const lang = {en:{label: "English",icon: en },vi:{label: "Tiếng Việt",icon: vi }};
 
 function LanguageSwitch() {
-  const [currentLang, setCurrentLang] = useState(en);
+  const [currentLang, setCurrentLang] = useState('en');
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleCloseUserMenu = () => {
+  const { t, i18n } = useTranslation()
+  const changeLanguageHandler = (lang) =>
+     {
+       i18n.changeLanguage(lang)
+       setCurrentLang(lang)
+     }
+  const handleCloseUserMenu = (e) => {
     setAnchorElUser(null);
   };
 
   const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
+   setAnchorElUser(event.currentTarget);
   };
   return (
     <>
-      <IconButton onMouseEnter={handleOpenUserMenu} sx={{ p: 0 }}>
-        <img src={en} width={32} />
+      <IconButton onMouseEnter={handleOpenUserMenu}  sx={{ p: 0 }}>
+        <img src={lang[currentLang].icon} width={32} />
       </IconButton>
       <Menu
         sx={{ mt: "45px" }}
@@ -38,8 +44,14 @@ function LanguageSwitch() {
         onClose={handleCloseUserMenu}
       >
         {Object.keys(lang).map((language) => (
-          <MenuItem key={language} onClick={handleCloseUserMenu}>
-            <ListItemIcon>
+          <MenuItem 
+            key={language} 
+            onClick={()=>{
+              handleCloseUserMenu()
+              changeLanguageHandler(language)
+            }}
+            >
+            <ListItemIcon >
                 <img src={lang[language].icon}/>
             </ListItemIcon>
             <ListItemText>{lang[language].label}</ListItemText>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from './translation/i18n';
+import { I18nextProvider } from 'react-i18next';
+
+window.currentLanguage = 'en'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+
   </React.StrictMode>
 );
 
