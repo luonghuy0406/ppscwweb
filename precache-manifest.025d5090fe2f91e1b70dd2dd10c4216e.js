b804eb88f3e7fb59fa60f235f69584e10da37cd6
Huy Nguyen <luonghuy0406@gmail.com>
2023-03-08 08:21:40 +0700

    Updates

---
+++ precache-manifest.025d5090fe2f91e1b70dd2dd10c4216e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "38bc370c4f1a8e7c989f",
+    "url": "/ppscwweb/static/css/main.fa88dcfc.chunk.css"
+  },
+  {
+    "revision": "38bc370c4f1a8e7c989f",
+    "url": "/ppscwweb/static/js/main.ed29e033.chunk.js"
+  },
+  {
+    "revision": "77a0f24e2d9346a54cb5",
+    "url": "/ppscwweb/static/js/runtime~main.586faf71.js"
+  },
+  {
+    "revision": "dd7e7513e18eb3cef942",
+    "url": "/ppscwweb/static/js/2.2baa6d5b.chunk.js"
+  },
+  {
+    "revision": "9eaaa9d598e25c799fd3",
+    "url": "/ppscwweb/static/js/3.814a2815.chunk.js"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/ppscwweb/static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "a57672b6cf96caf9cb03696506959b3a",
+    "url": "/ppscwweb/index.html"
+  }
+];
